018f45dbde125946f8aa3e5f63be5771b391bc18
gitthing03 <86090190+gitthing03@users.noreply.github.com>
2022-09-02 04:08:26 -0400

    Added start screen, pause screen, fixed idle animation
    Press escape to pause, screen only accepts mouse input atm, fixed a bug where idle animation wouldn't play while standing still

---
+++ Scripts/Menu.cs
@@ -0,0 +1,39 @@
+using Godot;
+using System;
+
+public class Menu : Control
+{
+	// Declare member variables here. Examples:
+	// private int a = 2;
+	// private string b = "text";
+
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		
+	}
+
+//  // Called every frame. 'delta' is the elapsed time since the previous frame.
+//  public override void _Process(float delta)
+//  {
+//      
+//  }
+
+	private void _on_StartButton_pressed() {
+	// start game
+		GetTree().ChangeScene("res://Scenes/Main.tscn");
+	}
+	
+	private void _on_QuitButton_pressed() {
+	// quit game
+		GetTree().Quit();
+	}
+	
+	// TODO: options
+}
+
+
+
+
+
+
