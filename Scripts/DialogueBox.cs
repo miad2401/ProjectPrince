24e15266473300e0b675e60f7773a740393817a0
gitthing03 <86090190+gitthing03@users.noreply.github.com>
2022-09-23 16:43:05 -0400

    Dialogue Boxes (in progress)

---
+++ Scripts/DialogueBox.cs
@@ -0,0 +1,325 @@
+using Godot;
+using System;
+
+public class DialogueBox : Control
+{
+	//Creates a signal to connect to PauseMenu
+	[Signal] public delegate void PauseForDialogue(bool pause);
+
+	/*
+	 * Export Fields
+	 * slideInTime - The time it takes for the Note to slide into view in seconds
+	 * slideOutTime - The time it takes for the Note to slide out of view in seconds
+	 * 
+	 * Constants
+	 * positionOffset - The amount of pixels to 
+	 * 
+	 * Fields
+	 * finalPosition - The relative position of the note when fully in view
+	 * hiddenPosition - The relative position of the note when not in view
+	 * NoteBackground - A reference to the NoteBackground node
+	 * slideIn - Bool for if the note is sliding in
+	 * slideOut - Bool for if the note is sliding out
+	 * slideProgress - The current progress that the note has slid in/out
+	 * noteActivated - Determines if the node has already been read
+	 */
+	
+	
+	[Export] bool startDialogue = true;
+	// spaceTextDelay - amount of time in seconds in takes for "press space to continue" to pop up on given line	
+	[Export] float spaceTextDelay = 4f;
+	// spaceTextBlinkSpeed - amount of time between blinks for the press space to continue prompt
+	[Export] float spaceTextBlinkSpeed = 0.75f;
+	// scrollSpeedScale - controls how fast text scrolls by, lower is slower, higher is faster (>1 makes no difference)
+	[Export] float scrollSpeedScale = 0.18f;
+	
+	// Used for scrolling text
+	int drawTextSpeed = 0;
+	int chatterLimit = 0;
+	float scrollSpeedSum;
+	
+	// Used to track when to show "press space to continue" prompt
+	float currentTextTime = 0;
+	float spaceTextTime = 0;
+	
+	
+	// Array of text lines
+	[Export] String[] textList;
+	// Array of who appears on left portrait
+	[Export] String[] leftPortraits;
+	// Array of who appears on right portrait
+	[Export] String[] rightPortraits;
+	
+	// TODO SHAKE LINES
+	
+	
+	
+	Container Left, Middle, Right;
+	Label Text, SpaceText;
+	CanvasLayer DBL;
+	
+	Sprite LeftPortrait, RightPortrait;
+	
+	
+	Texture Silas, Malcolm, Maisie, Daniel;
+	
+	// Original position reference to undo text shaking
+	Vector2 TextPosition;
+	
+	
+	Control PauseMenu;
+	
+	
+	// Total amount of time a shake takes (not fully implemented - only affects the "WAKEUP" convo)
+	/*float shakeTotalTime = 1.6f;
+	// Amount of time between each shake movement
+	float shakeBetweenTime;
+	float defaultShakeBetweenTime = 0.065f;
+	int currentShakeStrength;
+	int defaultShakeStrength = 9;
+	// Stores how many shake movements there are
+	int shakeMovements = 10;
+	// Keeps track of how long the text has shaken
+	float shakeTotalTimer = 0;
+	// Keeps track of how long the current movement has happened
+	float shakeBetweenTimer = 0f;
+	// Tracks current movement
+	int currentShakeMovement = 1;
+	// Flag that tracks if currently shaking
+	bool shaking = false;*/
+	
+	int currentText = 0;
+
+	
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		this.Visible = startDialogue;
+		Left = GetNode<Container>("DBL/Left");
+		Right = GetNode<Container>("DBL/Right");
+		Middle = GetNode<Container>("DBL/Middle");
+		Text = GetNode<Label>("DBL/Text");
+		SpaceText = GetNode<Label>("DBL/SpaceText");
+		SpaceText.Visible = false;
+		DBL = GetNode<CanvasLayer>("DBL");
+		//TextPosition = Text.GetPosition();
+		//Connects the PauseForDialogue signal to the PauseMenu
+		Connect(nameof(PauseForDialogue), GetNode("/root/Main/GUI/PauseMenu"), "PauseFromOutsidePauseMenu");
+		PauseMenu = GetNode<Control>("/root/Main/GUI/PauseMenu");
+		if (startDialogue) {
+			EmitSignal(nameof(PauseForDialogue), true);
+		}
+		Text.Text = textList[currentText];
+		chatterLimit = textList[currentText].Length;
+		
+		// Load portraits
+		Silas = ResourceLoader.Load("res://Art/Portraits/silas.png") as Texture;
+		Daniel = ResourceLoader.Load("res://Art/Portraits/daniel.png") as Texture;
+		Malcolm = ResourceLoader.Load("res://Art/Portraits/malcolm.png") as Texture;
+		Maisie = ResourceLoader.Load("res://Art/Portraits/maisie.png") as Texture;
+		LeftPortrait = GetNode<Sprite>("DBL/Left/Portrait/Sprite");
+		RightPortrait = GetNode<Sprite>("DBL/Right/Portrait/Sprite");
+		
+		
+		// Starting left portrait
+		if (leftPortraits[currentText] != "" && leftPortraits[currentText] != "hidden") {
+			if (leftPortraits[currentText] == "Silas"){
+				LeftPortrait.Texture = Silas;
+			}
+			else if (leftPortraits[currentText] == "Daniel") {
+				LeftPortrait.Texture = Daniel;
+			}
+			else if (leftPortraits[currentText] == "Malcolm") {
+				LeftPortrait.Texture = Malcolm;
+			}
+			else {
+				LeftPortrait.Texture = Maisie;
+			}
+		}
+		else if (leftPortraits[currentText] == "hidden"){
+			LeftPortrait.Visible = false;
+		}
+		else {
+			Left.Visible = false;
+		}
+		// Starting right portrait
+		if (rightPortraits[currentText] != "" && rightPortraits[currentText] != "hidden") {
+			if (rightPortraits[currentText] == "Silas"){
+				RightPortrait.Texture = Silas;
+			}
+			else if (rightPortraits[currentText] == "Daniel") {
+				RightPortrait.Texture = Daniel;
+			}
+			else if (rightPortraits[currentText] == "Malcolm") {
+				RightPortrait.Texture = Malcolm;
+			}
+			else {
+				RightPortrait.Texture = Maisie;
+			}
+		}
+		else if (rightPortraits[currentText] == "hidden"){
+			RightPortrait.Visible = false;
+		}
+		else {
+			Right.Visible = false;
+		}
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(float delta) {
+		if (DBL.Visible) {
+			if (Input.IsActionJustPressed("attack")) {
+				// If not at end of dialogue
+				if (currentText != (textList.Length-1)) {
+					// Stop any possible shakes
+					//stopShake();
+					// Hide text
+					Text.Visible = false;
+					// Reset text position
+					//CutsceneText.SetPosition(TextPosition);
+					// Advance text var
+					currentText++;
+					// Reset space prompt timer
+					currentTextTime = 0;
+					// Reset scroller
+					drawTextSpeed = 0;
+					Text.VisibleCharacters = 0;
+					// Hide space prompt
+					SpaceText.Visible = false;
+					// Reset scroller
+					chatterLimit = textList[currentText].Length;
+					// Ready text
+					Text.Text = textList[currentText];
+					// Shake for wakeup convo
+					/*if (currentText == 5 || currentText == 6) {
+						shaking = true;
+					}*
+					// Slow text afterwards
+					else if (currentText == 7) {
+						scrollSpeedScale /= 1.85f;
+					}
+					// Bring text back to normal speed
+					else if (currentText == 8) {
+						scrollSpeedScale *= 1.85f;
+					}
+					// Shake longer but less violently when knighs come
+					else if (currentText == 40 || currentText == 42) {
+						shaking = true;
+						currentShakeStrength = 4;
+						shakeTotalTime *= 3;
+					}
+					else if (currentText == 41) {
+						shakeTotalTime /=3;
+					}*/
+					// Make text visible
+					Text.Visible = true;
+				}
+				// End of dialogue, play game
+				else {
+					EmitSignal(nameof(PauseForDialogue), false);
+					DBL.Visible = false;
+				}
+				// check for portrait, if so load
+				if (leftPortraits[currentText] != "" && leftPortraits[currentText] != "hidden") {
+					if (leftPortraits[currentText] == "Silas"){
+						LeftPortrait.Texture = Silas;
+					}
+					else if (leftPortraits[currentText] == "Daniel") {
+						LeftPortrait.Texture = Daniel;
+					}
+					else if (leftPortraits[currentText] == "Malcolm") {
+						LeftPortrait.Texture = Malcolm;
+					}
+					else {
+						LeftPortrait.Texture = Maisie;
+					}
+				}
+				else if (leftPortraits[currentText] == "hidden"){
+					LeftPortrait.Visible = false;
+				}
+				else {
+					Left.Visible = false;
+				}
+			}
+			// If esc is pressed, close dialogue
+			else if (Input.IsActionPressed("pause")) {
+				EmitSignal(nameof(PauseForDialogue), false);
+				DBL.Visible = false;
+			}
+			// Process
+			else {
+				// If time has elapsed to show space prompt
+				if (currentTextTime > spaceTextDelay) {
+					// Blinking space prompt
+					if (spaceTextTime > spaceTextBlinkSpeed) {
+						spaceTextTime = 0;
+						SpaceText.Visible = !SpaceText.Visible;
+					}
+					else {
+						spaceTextTime += delta;
+					}
+				}
+				else {
+					currentTextTime += delta;
+				}
+			}
+			
+			// Scroll text
+			scrollSpeedSum += (delta*scrollSpeedScale);
+			if (scrollSpeedSum > delta) {
+				scrollSpeedSum = 0;
+				showChatter();
+			}
+			
+			/*// Shake text if applicable
+			if (shaking) {
+				shakeTotalTimer += delta;
+				if (shakeTotalTimer < shakeTotalTime) {
+					shakeBetweenTimer += delta;
+					if (shakeBetweenTimer > shakeBetweenTime) {
+						if (currentShakeMovement == 1 || currentShakeMovement == 5 || currentShakeMovement == 8){
+							CutsceneText.SetPosition(new Vector2(CutsceneText.GetPosition().x+currentShakeStrength,CutsceneText.GetPosition().y));
+						}
+						else if (currentShakeMovement == 2 || currentShakeMovement == 4){
+							CutsceneText.SetPosition(new Vector2(CutsceneText.GetPosition().x,CutsceneText.GetPosition().y+currentShakeStrength));
+						}
+						else if (currentShakeMovement == 3 || currentShakeMovement == 6 || currentShakeMovement == 10) {
+							CutsceneText.SetPosition(new Vector2(CutsceneText.GetPosition().x-currentShakeStrength,CutsceneText.GetPosition().y));
+						}
+						else {
+							CutsceneText.SetPosition(new Vector2(CutsceneText.GetPosition().x,CutsceneText.GetPosition().y-currentShakeStrength));
+						}
+						currentShakeMovement++;
+						if (currentShakeMovement > 10) {
+							currentShakeMovement = 0;
+						}
+						shakeBetweenTimer = 0;
+					}
+				}
+				else {
+					//stopShake();
+				}
+			}*/
+		}
+	}
+	
+	// Scroll function
+	public void showChatter() {
+		if (drawTextSpeed < chatterLimit) {
+			drawTextSpeed++;
+			Text.VisibleCharacters = drawTextSpeed;
+		}
+	}
+
+	//Called when the area is entered
+	public void OnNoteBodyEntered(Node body)
+	{
+		//Checks if the touched object was a player
+		if (body.IsInGroup("Player"))
+		{
+			//Send a Signal to PauseMenu to pause the game
+			EmitSignal(nameof(PauseForDialogue), true);
+			this.Visible = true;
+		}
+	}
+}
--- Scripts/Note.cs
+++ Scripts/Note.cs
@@ -3,121 +3,121 @@ using System;
 
 public class Note : Control
 {
-    //Creates a signal to connect to PauseMenu
-    [Signal] public delegate void PauseForNote(bool pause);
+	//Creates a signal to connect to PauseMenu
+	[Signal] public delegate void PauseForNote(bool pause);
 
-    /*
-     * Export Fields
-     * slideInTime - The time it takes for the Note to slide into view in seconds
-     * slideOutTime - The time it takes for the Note to slide out of view in seconds
-     * 
-     * Constants
-     * positionOffset - The amount of pixels to 
-     * 
-     * Fields
-     * finalPosition - The relative position of the note when fully in view
-     * hiddenPosition - The relative position of the note when not in view
-     * NoteBackground - A reference to the NoteBackground node
-     * slideIn - Bool for if the note is sliding in
-     * slideOut - Bool for if the note is sliding out
-     * slideProgress - The current progress that the note has slid in/out
-     * noteActivated - Determines if the node has already been read
-     */
-    [Export] float slideInTime;
-    [Export] float slideOutTime;
-    [Export] bool ExitHint;
+	/*
+	 * Export Fields
+	 * slideInTime - The time it takes for the Note to slide into view in seconds
+	 * slideOutTime - The time it takes for the Note to slide out of view in seconds
+	 * 
+	 * Constants
+	 * positionOffset - The amount of pixels to 
+	 * 
+	 * Fields
+	 * finalPosition - The relative position of the note when fully in view
+	 * hiddenPosition - The relative position of the note when not in view
+	 * NoteBackground - A reference to the NoteBackground node
+	 * slideIn - Bool for if the note is sliding in
+	 * slideOut - Bool for if the note is sliding out
+	 * slideProgress - The current progress that the note has slid in/out
+	 * noteActivated - Determines if the node has already been read
+	 */
+	[Export] float slideInTime;
+	[Export] float slideOutTime;
+	[Export] bool ExitHint;
 
-    const int positionOffset = 254;
+	const int positionOffset = 254;
 
-    float finalPosition;
-    float hiddenPosition;
-    TextureRect NoteBackground;
-    bool slideIn = false;
-    bool slideOut = false;
-    float slideProgress = 0;
-    bool noteActivated = false;
+	float finalPosition;
+	float hiddenPosition;
+	TextureRect NoteBackground;
+	bool slideIn = false;
+	bool slideOut = false;
+	float slideProgress = 0;
+	bool noteActivated = false;
 
-    // Called when the node enters the scene tree for the first time.
-    public override void _Ready()
-    {
-        //Connects the PauseForNote signal to the PauseMenu
-        Connect(nameof(PauseForNote), GetNode("/root/Main/GUI/PauseMenu"), "PauseFromOutsidePauseMenu");
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		//Connects the PauseForNote signal to the PauseMenu
+		Connect(nameof(PauseForNote), GetNode("/root/Main/GUI/PauseMenu"), "PauseFromOutsidePauseMenu");
 
-        //Creates the reference for the NoteBackground and makes it invisible
-        NoteBackground = GetNode<TextureRect>("Note/NoteBackground");
-        NoteBackground.Visible = false;
+		//Creates the reference for the NoteBackground and makes it invisible
+		NoteBackground = GetNode<TextureRect>("Note/NoteBackground");
+		NoteBackground.Visible = false;
 
-        //If ExitHint is enabled, makes it visible
-        if (ExitHint)
-        {
-            GetNode<Panel>("Note/NoteBackground/TextureButton/ExitHint").Visible = true;
-        }
+		//If ExitHint is enabled, makes it visible
+		if (ExitHint)
+		{
+			GetNode<Panel>("Note/NoteBackground/TextureButton/ExitHint").Visible = true;
+		}
 
-        //Sets the finalPosition to the current NoteBackground's y position
-        finalPosition = NoteBackground.RectPosition.y;
-        //Sets the hiddenPosition from the finalPosition and how far the offset is to make it off screen
-        hiddenPosition = finalPosition + positionOffset;
-    }
+		//Sets the finalPosition to the current NoteBackground's y position
+		finalPosition = NoteBackground.RectPosition.y;
+		//Sets the hiddenPosition from the finalPosition and how far the offset is to make it off screen
+		hiddenPosition = finalPosition + positionOffset;
+	}
 
-    // Called every frame. 'delta' is the elapsed time since the previous frame.
-    public override void _Process(float delta)
-    {
-        //If the note is sliding in
-        if (slideIn)
-        {
-            //Add progress to slideProgress
-            slideProgress += delta;
-            //The NoteBackground x position is untouched, but the y position changes
-            //The y becomes a value between hidden and finalPosition based on slideProgress and the speed of the note's movement
-            NoteBackground.RectPosition = new Vector2(NoteBackground.RectPosition.x, hiddenPosition - slideProgress*((hiddenPosition-finalPosition)/slideInTime));
-            //If the Note has reached the center of the screen
-            if(NoteBackground.RectPosition.y <= finalPosition)
-            {
-                //Stop sliding in, reset the slide value, and hide the note sprite
-                slideIn = false;
-                slideProgress = 0;
-                GetNode<Sprite>("Area2D/Sprite").Visible = false;
-            }
-        }
-        else if (slideOut)
-        {
-            //Add progress to slideProgress
-            slideProgress += delta;
-            //The NoteBackground x position is untouched, but the y position changes
-            //The y becomes a value between hidden and finalPosition based on slideProgress and the speed of the note's movement
-            NoteBackground.RectPosition = new Vector2(NoteBackground.RectPosition.x, finalPosition + slideProgress * ((hiddenPosition - finalPosition) / slideOutTime));
-            //If the Note has left the screen
-            if (NoteBackground.RectPosition.y > hiddenPosition)
-            {
-                //Stop sliding out
-                slideOut = false;
-                //Queuefree as the note will not be used again
-                QueueFree();
-            }
-        }
-    }
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(float delta)
+	{
+		//If the note is sliding in
+		if (slideIn)
+		{
+			//Add progress to slideProgress
+			slideProgress += delta;
+			//The NoteBackground x position is untouched, but the y position changes
+			//The y becomes a value between hidden and finalPosition based on slideProgress and the speed of the note's movement
+			NoteBackground.RectPosition = new Vector2(NoteBackground.RectPosition.x, hiddenPosition - slideProgress*((hiddenPosition-finalPosition)/slideInTime));
+			//If the Note has reached the center of the screen
+			if(NoteBackground.RectPosition.y <= finalPosition)
+			{
+				//Stop sliding in, reset the slide value, and hide the note sprite
+				slideIn = false;
+				slideProgress = 0;
+				GetNode<Sprite>("Area2D/Sprite").Visible = false;
+			}
+		}
+		else if (slideOut)
+		{
+			//Add progress to slideProgress
+			slideProgress += delta;
+			//The NoteBackground x position is untouched, but the y position changes
+			//The y becomes a value between hidden and finalPosition based on slideProgress and the speed of the note's movement
+			NoteBackground.RectPosition = new Vector2(NoteBackground.RectPosition.x, finalPosition + slideProgress * ((hiddenPosition - finalPosition) / slideOutTime));
+			//If the Note has left the screen
+			if (NoteBackground.RectPosition.y > hiddenPosition)
+			{
+				//Stop sliding out
+				slideOut = false;
+				//Queuefree as the note will not be used again
+				QueueFree();
+			}
+		}
+	}
 
-    //Called when the area is entered
-    public void OnNoteBodyEntered(Node body)
-    {
-        //Checks if the touched object was a player
-        if (body.IsInGroup("Player") && !noteActivated)
-        {
-            //Send a Signal to PauseMenu to pause the game
-            EmitSignal(nameof(PauseForNote), true);
-            //Move the NoteBackgroudn to be juuuust off-screen then make it visible
-            NoteBackground.RectPosition = new Vector2(NoteBackground.RectPosition.x, hiddenPosition);
-            NoteBackground.Visible = true;
-            //Start sliding in the note to the center of the screen
-            slideIn = true;
-            //Mark the note as activated
-            noteActivated = true;
-        }
-    }
-    public virtual void OnExitNotePressed()
-    {
-        slideOut = true;
-        //Tells PauseMenu to unpause game
-        EmitSignal(nameof(PauseForNote), false);
-    }
+	//Called when the area is entered
+	public void OnNoteBodyEntered(Node body)
+	{
+		//Checks if the touched object was a player
+		if (body.IsInGroup("Player") && !noteActivated)
+		{
+			//Send a Signal to PauseMenu to pause the game
+			EmitSignal(nameof(PauseForNote), true);
+			//Move the NoteBackgroudn to be juuuust off-screen then make it visible
+			NoteBackground.RectPosition = new Vector2(NoteBackground.RectPosition.x, hiddenPosition);
+			NoteBackground.Visible = true;
+			//Start sliding in the note to the center of the screen
+			slideIn = true;
+			//Mark the note as activated
+			noteActivated = true;
+		}
+	}
+	public virtual void OnExitNotePressed()
+	{
+		slideOut = true;
+		//Tells PauseMenu to unpause game
+		EmitSignal(nameof(PauseForNote), false);
+	}
 }
