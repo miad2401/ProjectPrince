1148d84cfc12a092f952c8dcb5dea6f92f73face
gitthing03 <86090190+gitthing03@users.noreply.github.com>
2022-09-23 21:41:20 -0400

    Dialogue Box instantiation

--- Scripts/Menu.cs
+++ Scripts/Menu.cs
@@ -3,13 +3,13 @@ using System;
 
 public class Menu : Control
 {	
-	private void _on_StartButton_pressed()
+	public void _on_StartButton_pressed()
 	{
 		// GetTree().ChangeScene("res://scenes/OpeningCutscene.tscn");
 		GetTree().ChangeScene("res://Scenes/Main.tscn");
 	}
 	
-	private void _on_QuitButton_pressed() {
+	public void _on_QuitButton_pressed() {
 	// quit game
 		GetTree().Quit();
 	}
